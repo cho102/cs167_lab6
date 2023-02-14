0dac938f38c8bae2bb9fc8aca035976cc8de642f
Cindy Ho <cho102@ucr.edu>
2023-02-14 20:07:20 +0000

    finished part b

--- cho102_lab6/src/main/scala/edu/ucr/cs/cs167/cho102/AppSQL.scala
+++ cho102_lab6/src/main/scala/edu/ucr/cs/cs167/cho102/AppSQL.scala
@@ -1,7 +1,7 @@
 package edu.ucr.cs.cs167.cho102
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, sql}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object AppSQL {
 
@@ -17,18 +17,91 @@ object AppSQL {
       .config(conf)
       .getOrCreate()
 
+    val command: String = args(0)
+    val inputfile: String = args(1)
     try {
-      // Your code will go here
       val input = spark.read.format("csv")
         .option("sep", "\t")
-       // .option("inferSchema", "true")
+        .option("inferSchema", "true")
         .option("header", "true")
-        .load("nasa_19950801.tsv")
-
+        .load(inputfile)
       import spark.implicits._
+      input.createOrReplaceTempView("log_lines")
+      val t1 = System.nanoTime
+      var validCommand = true
+      command match {
+        case "count-all" =>
+          // Count total number of records in the file
+          val query: String = "SELECT count(*) FROM log_lines;"// TODO 9: write SQL query to count the total number of rows in `log_lines`
+          val count: Long = spark.sql(query).first().getAs[Long](0)
+          println(s"Total count for file '$inputfile' is $count")
+        case "code-filter" =>
+          // Filter the file by response code, args(2), and print the total number of matching lines
+          val responseCode: String = args(2)
+          val query: String = "SELECT count(*) FROM log_lines WHERE response=" + responseCode + ";"// TODO 10: write SQL query to filter view `log_lines` by `responseCode`
+          val count: Long = spark.sql(query).first().getAs[Long](0)
+          println(s"Total count for file '$inputfile' with response code $responseCode is $count")
+        case "time-filter" =>
+          // Filter by time range [from = args(2), to = args(3)], and print the total number of matching lines
+          val from: Long = args(2).toLong
+          val to: Long = args(3).toLong
+          val query: String = "SELECT count(*) FROM log_lines WHERE time BETWEEN " + from + " AND " + to + ";"// TODO 11: write SQL query to filter view `log_lines` by `time` between `from` and `to`
+          val count: Long = spark.sql(query).first().getAs[Long](0)
+          println(s"Total count for file '$inputfile' in time range [$from, $to] is $count")
+        case "count-by-code" =>
+          // Group the lines by response code and count the number of records per group
+          println(s"Number of lines per code for the file '$inputfile'")
+          println("Code,Count")
+          val query: String = "SELECT response, COUNT(bytes) FROM log_lines GROUP BY response ORDER BY response;"// TODO 12: write SQL query the get the count for each `response`, results must be ordered by `response` in ascending order
+            spark.sql(query).foreach(row => println(s"${row.get(0)},${row.get(1)}"))
+        case "sum-bytes-by-code" =>
+          // Group the lines by response code and sum the total bytes per group
+          println(s"Total bytes per code for the file '$inputfile'")
+          println("Code,Sum(bytes)")
+          val query: String = "SELECT response, SUM(bytes) FROM log_lines GROUP BY response ORDER BY response;"// TODO 13: write SQL query the get the sum of `bytes` for each `response`, results must be ordered by `response` in ascending order
+            spark.sql(query).foreach(row => println(s"${row.get(0)},${row.get(1)}"))
+        case "avg-bytes-by-code" =>
+          // Group the liens by response code and calculate the average bytes per group
+          println(s"Average bytes per code for the file '$inputfile'")
+          println("Code,Avg(bytes)")
+          val query: String = "SELECT response, AVG(bytes) FROM log_lines GROUP BY response ORDER BY response;"// TODO 14: write SQL query the get the average of `bytes` for each `response`, results must be ordered by `response` in ascending order
+            spark.sql(query).foreach(row => println(s"${row.get(0)},${row.get(1)}"))
+        case "top-host" =>
+          // print the host the largest number of lines and print the number of lines
+          println(s"Top host in the file '$inputfile' by number of entries")
+          val query: String = "SELECT host, COUNT(*) AS cnt FROM log_lines GROUP BY host ORDER BY cnt DESC LIMIT 1;"// TODO 15: write SQL query the get the `host` with the largest count
+          val topHost: Row = spark.sql(query).first()
+          println(s"Host: ${topHost.get(0)}")
+          println(s"Number of entries: ${topHost.get(1)}")
+        case "comparison" =>
+          // Given a specific time, calculate the number of lines per response code for the
+          // entries that happened before that time, and once more for the lines that happened at or after
+          // that time. Print them side-by-side in a tabular form.
 
-      input.show()
-      input.printSchema()
+          // TODO 16a: comment the following line
+          println("Not implemented")
+
+        // TODO 16b: uncomment the following 3 lines
+        // val filterTimestamp: Long = args(2).toLong
+        // println(s"Comparison of the number of lines per code before and after $filterTimestamp on file '$inputfile'")
+        // println("Code,CountBefore,CountAfter")
+
+        // TODO 16c: Uncomment the following lines and complete your function here if you prefer to use SQL queries
+        // val query: String = // Write your query here. Results must be ordered by `response` in ascending order
+        // spark.sql(query).foreach(row => println(s"${row.get(0)},${row.get(1)},${row.get(2)}"))
+
+        // TODO 16d: Uncomment the following lines and complete your function here if you prefer to use DataFrame
+        // val countsBefore: DataFrame = // on `input`
+        // val countsAfter: DataFrame = // on `input`
+        // val comparedResults: DataFrame = countsBefore.join(countsAfter, "response").orderBy("response")
+        // comparedResults.foreach(row => println(s"${row.get(0)},${row.get(1)},${row.get(2)}"))
+        case _ => validCommand = false
+      }
+      val t2 = System.nanoTime
+      if (validCommand)
+        println(s"Command '$command' on file '$inputfile' finished in ${(t2 - t1) * 1E-9} seconds")
+      else
+        Console.err.println(s"Invalid command '$command'")
     } finally {
       spark.stop
     }
